f59e9fc32113f77640b8c57e2b39a99857bc808f
Ross <rstepanyak@screenmeet.com>
2022-05-11 16:57:07 +0300

    change image to cvpixelbuffer convertion

--- ScreenMeetSDK/Classes/Transport/Channels/SMMediasoupChannel/Tracks & Encodings/SMTracksManager.swift
+++ ScreenMeetSDK/Classes/Transport/Channels/SMMediasoupChannel/Tracks & Encodings/SMTracksManager.swift
@@ -9,7 +9,7 @@ import UIKit
 import WebRTC
 
 class SMTracksManager: NSObject {
-    var videoSourceDevice: AVCaptureDevice?    
+    var videoSourceDevice: AVCaptureDevice?
     private var mediaStream: RTCMediaStream!
     private var videoSource: RTCVideoSource!
     private var videoTrack: RTCVideoTrack!
@@ -27,7 +27,7 @@ class SMTracksManager: NSObject {
         videoSource = factory.videoSource()
         
         videoTrack = factory.videoTrack(with: videoSource, trackId: "ARDAMSv0")
-        self.mediaStream.addVideoTrack(videoTrack)        
+        self.mediaStream.addVideoTrack(videoTrack)
         return videoTrack
     }
     
@@ -171,61 +171,17 @@ class SMTracksManager: NSObject {
             let rotation = RTCVideoRotation._0
             let timeStampNs: Int64 = Int64(Date().timeIntervalSince1970 * 1000000000)
             
-            let rtcpixelBuffer = RTCCVPixelBuffer(pixelBuffer: pixelBufferFromImage(image))
-            let frame = RTCVideoFrame(buffer:rtcpixelBuffer, rotation: rotation, timeStampNs: timeStampNs)
-            self.capturer(videoCapturer, didCapture: frame)
+            if let pixelBuffer =  buffer(from:(image)) {
+                let rtcpixelBuffer = RTCCVPixelBuffer(pixelBuffer: pixelBuffer)
+                let frame = RTCVideoFrame(buffer:rtcpixelBuffer, rotation: rotation, timeStampNs: timeStampNs)
+                if videoCapturer != nil {
+                    self.capturer(videoCapturer, didCapture: frame)
+                }
+            }
+            
         }
         return handler
     }
-    
-    func pixelBufferFromImage(_ image: UIImage) -> CVPixelBuffer {
-            let ciimage = CIImage(image: image)
-            //let cgimage = convertCIImageToCGImage(inputImage: ciimage!)
-            let tmpcontext = CIContext(options: nil)
-            let cgimage =  tmpcontext.createCGImage(ciimage!, from: ciimage!.extent)
-            
-            let cfnumPointer = UnsafeMutablePointer<UnsafeRawPointer>.allocate(capacity: 1)
-            let cfnum = CFNumberCreate(kCFAllocatorDefault, .intType, cfnumPointer)
-            let keys: [CFString] = [kCVPixelBufferCGImageCompatibilityKey, kCVPixelBufferCGBitmapContextCompatibilityKey, kCVPixelBufferBytesPerRowAlignmentKey]
-            let values: [CFTypeRef] = [kCFBooleanTrue, kCFBooleanTrue, cfnum!]
-            let keysPointer = UnsafeMutablePointer<UnsafeRawPointer?>.allocate(capacity: 1)
-            let valuesPointer =  UnsafeMutablePointer<UnsafeRawPointer?>.allocate(capacity: 1)
-            keysPointer.initialize(to: keys)
-            valuesPointer.initialize(to: values)
-            
-            let options = CFDictionaryCreate(kCFAllocatorDefault, keysPointer, valuesPointer, keys.count, nil, nil)
-           
-            let width = cgimage!.width
-            let height = cgimage!.height
-         
-            var pxbuffer: CVPixelBuffer?
-            // if pxbuffer = nil, you will get status = -6661
-            var status = CVPixelBufferCreate(kCFAllocatorDefault, width, height,
-                                             kCVPixelFormatType_32BGRA, options, &pxbuffer)
-            status = CVPixelBufferLockBaseAddress(pxbuffer!, CVPixelBufferLockFlags(rawValue: 0));
-            
-            let bufferAddress = CVPixelBufferGetBaseAddress(pxbuffer!);
-
-            
-            let rgbColorSpace = CGColorSpaceCreateDeviceRGB();
-            let bytesperrow = CVPixelBufferGetBytesPerRow(pxbuffer!)
-            let context = CGContext(data: bufferAddress,
-                                    width: width,
-                                    height: height,
-                                    bitsPerComponent: 8,
-                                    bytesPerRow: bytesperrow,
-                                    space: rgbColorSpace,
-                                    bitmapInfo: CGImageAlphaInfo.premultipliedFirst.rawValue | CGBitmapInfo.byteOrder32Little.rawValue);
-            context?.concatenate(CGAffineTransform(rotationAngle: 0))
-            //context?.concatenate(__CGAffineTransformMake( 1, 0, 0, -1, 0, CGFloat(height) )) //Flip Vertical
-    //        context?.concatenate(__CGAffineTransformMake( -1.0, 0.0, 0.0, 1.0, CGFloat(width), 0.0)) //Flip Horizontal
-            
-
-            context?.draw(cgimage!, in: CGRect(x:0, y:0, width:CGFloat(width), height:CGFloat(height)));
-            status = CVPixelBufferUnlockBaseAddress(pxbuffer!, CVPixelBufferLockFlags(rawValue: 0));
-            return pxbuffer!;
-            
-        }
     
     private func buffer(from image: UIImage) -> CVPixelBuffer? {
         let attrs = [kCVPixelBufferCGImageCompatibilityKey: kCFBooleanTrue, kCVPixelBufferCGBitmapContextCompatibilityKey: kCFBooleanTrue] as CFDictionary
